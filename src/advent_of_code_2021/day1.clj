393ad26bd385bdd73c06fac62ebe81af29b98737
Trevor Hartman <trevorhartman@gmail.com>
2021-12-01 22:31:08 -0700

    Remove assoc

--- src/advent_of_code_2021/day1.clj
+++ src/advent_of_code_2021/day1.clj
@@ -1,18 +1,14 @@
-(ns advent-of-code-2021.day1
-  (:require [clojure.string :as string]))
+(ns advent-of-code-2021.day1 (:require [clojure.string :as string]))
 
 (def input
   (->> (slurp "https://gist.githubusercontent.com/devth/d6a65b1377071ffd0f56b9e539266db2/raw/654dbb61ce4f9f8329212a13cb48fa203d6fdd36/aoc-2021-day1-input")
        string/split-lines (map read-string)))
 
-(defn count-incd [coll]
-  (:incd (reduce
-          (fn [{:keys [incd prev-line] :as acc} line]
-            (assoc acc
-                   :incd (if (and prev-line (> line prev-line)) (inc incd) incd)
-                   :prev-line line))
-          {:incd 0 :prev-line nil}
-          coll)))
+(defn count-incd [[f & remaining]]
+  (:incd (reduce (fn [{:keys [incd prev-line]} line]
+                   {:incd (if (> line prev-line) (inc incd) incd)
+                    :prev-line line})
+                 {:incd 0 :prev-line f} remaining)))
 
 (def part1 (count-incd input))
 
