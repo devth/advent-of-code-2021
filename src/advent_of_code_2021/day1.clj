641d8e880bfc460712e2801b229f127a2ea5706d
Trevor Hartman <trevorhartman@gmail.com>
2021-12-01 22:10:01 -0700

    Remove local resources

--- src/advent_of_code_2021/day1.clj
+++ src/advent_of_code_2021/day1.clj
@@ -1,20 +1,18 @@
 (ns advent-of-code-2021.day1
-  (:require
-   [clojure.string :as string]
-   [clojure.java.io :as io]))
+  (:require [clojure.string :as string]))
 
 (def input
-  (->> (io/resource "data/day-1-input.txt")
-       slurp string/split-lines (map read-string)))
+  (->> (slurp "https://gist.githubusercontent.com/devth/d6a65b1377071ffd0f56b9e539266db2/raw/654dbb61ce4f9f8329212a13cb48fa203d6fdd36/aoc-2021-day1-input")
+       string/split-lines (map read-string)))
 
 (defn count-incd [coll]
-  (:incd
-   (reduce
-    (fn [{:keys [incd prev-line] :as acc} line]
-      (assoc acc :incd (if (and prev-line (> line prev-line)) (inc incd) incd)
-             :prev-line line))
-    {:incd 0 :prev-line nil}
-    coll)))
+  (:incd (reduce
+          (fn [{:keys [incd prev-line] :as acc} line]
+            (assoc acc
+                   :incd (if (and prev-line (> line prev-line)) (inc incd) incd)
+                   :prev-line line))
+          {:incd 0 :prev-line nil}
+          coll)))
 
 (def part1 (count-incd input))
 
